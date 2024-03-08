869ac7f75bcd1360b34326ac689808467c21412a
Adarsha-Nayaka <kadarshanayaka2@gmail.com>
2024-03-08 17:30:00 -0500

    Your descriptive commit message here

--- src/components/Budget.js
+++ src/components/Budget.js
@@ -1,1 +1,46 @@
+import React, { useContext, useState } from 'react';
+import { AppContext } from '../context/AppContext';
 
+const Budget = () => {
+    const { budget, expenses } = useContext(AppContext);
+    const [newBudget, setNewBudget] = useState(budget);
+    const totalExpenses = expenses.reduce((total, item) => total + item.cost, 0);
+    const [currency, setCurrency] = useState('£'); // Default currency is set to Pound
+
+    const handleCurrencyChange = (event) => {
+        setCurrency(event.target.value);
+    }
+
+    const handleBudgetChange = (event) => {
+        const value = parseFloat(event.target.value); // Convert input value to a number
+        if (!isNaN(value) && value >= totalExpenses) {
+            setNewBudget(value);
+        } else {
+            alert("Budget cannot be less than total expenses.");
+        }
+    }
+
+    return (
+        <div className='alert alert-secondary'>
+            <span>Currency: </span>
+            <select className="custom-select" style={{ width: 'auto', marginLeft: '10px' }} value={currency} onChange={handleCurrencyChange}>
+                <option value="$">$ Dollar</option>
+                <option value="£">£ Pound</option>
+                <option value="€">€ Euro</option>
+                <option value="₹">₹ Rupee</option>
+            </select>
+            <br />
+            <br />
+            <span>Budget: </span>
+            <div className="input-group mb-3">
+                <div className="input-group-prepend">
+                    <span className="input-group-text">{currency}</span>
+                </div>
+                <input type="number" step="10" min={totalExpenses} className="form-control" value={newBudget} onChange={handleBudgetChange} />
+            </div>
+            <span>Total Expenses: {currency}{totalExpenses}</span>
+        </div>
+    );
+};
+
+export default Budget;
--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,1 +1,73 @@
+import React, { useContext } from 'react';
+import { TiDelete } from 'react-icons/ti';
+import { AppContext } from '../context/AppContext';
 
+const ExpenseList = () => {
+    const { expenses } = useContext(AppContext);
+    const { selectedCurrency } = useContext(AppContext); // Get selected currency from context
+
+    return (
+        <table className='table'>
+            <thead className="thead-light">
+                <tr>
+                    <th scope="col">Department</th>
+                    <th scope="col">Allocated Budget</th>
+                    <th scope="col">Increase by 10</th>
+                    <th scope="col">Decrease by 10</th>
+                    <th scope="col">Delete</th>
+                </tr>
+            </thead>
+            <tbody>
+                {expenses.map((expense) => (
+                    <ExpenseItem id={expense.id} key={expense.id} name={expense.name} cost={expense.cost} selectedCurrency={selectedCurrency} />
+                ))}
+            </tbody>
+        </table>
+    );
+};
+
+const ExpenseItem = (props) => {
+    const { dispatch } = useContext(AppContext);
+    const { selectedCurrency } = useContext(AppContext); // Get selected currency from context
+
+    const handleDeleteExpense = () => {
+        dispatch({
+            type: 'DELETE_EXPENSE',
+            payload: props.id,
+        });
+    };
+
+    const increaseAllocation = (name) => {
+        const expense = {
+            name: name,
+            cost: 10,
+        };
+        dispatch({
+            type: 'ADD_EXPENSE',
+            payload: expense
+        });
+    }
+
+    const decreaseAllocation = (name) => {
+        const expense = {
+            name: name,
+            cost: -10, // Decrease by 10
+        };
+        dispatch({
+            type: 'ADD_EXPENSE',
+            payload: expense
+        });
+    }
+
+    return (
+        <tr>
+            <td>{props.name}</td>
+            <td>{selectedCurrency === 'pound' ? '£' : selectedCurrency === 'dollar' ? '$' : selectedCurrency === 'euro' ? '€' : '₹'}{props.cost}</td>
+            <td><button onClick={event => increaseAllocation(props.name)}>+</button></td>
+            <td><button onClick={event => decreaseAllocation(props.name)}>-</button></td>
+            <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
+        </tr>
+    );
+};
+
+export default ExpenseList;
